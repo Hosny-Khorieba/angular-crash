17bf84cb0f90ef476cd75294ad7a495e473d26cf
Hosny Emad Khorieba <hosnykhorieba@gmail.com>
2023-11-04 22:26:52 +0200

    finished

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-add-task',
@@ -13,10 +15,15 @@ export class AddTaskComponent implements OnInit {
   text: string;
   day: string;
   reminder: boolean = false;
+  showAddTask: boolean;
+  subscription: Subscription;
 
   ngOnInit(): void {}
 
-  constructor(){}
+  constructor(private uiService: UiService){
+    this.subscription = uiService.onToggle()
+    .subscribe(v => this.showAddTask = v);
+  }
 
   onSubmit(){
     const newTask = {
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit{
-  title : string = 'Task Tracker';
+export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker';
+  showAddTask: boolean;
+  subscription: Subscription;
 
-  constructor(){}
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = uiService.onToggle()
+      .subscribe(v => this.showAddTask = v);
+  }
 
-  ngOnInit(): void {}
-  
-  toggleAddTask(){
-    console.log('toggle');
+  ngOnInit(): void { }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
